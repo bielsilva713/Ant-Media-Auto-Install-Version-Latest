f5ce8f5f85318d2b4a6e370b0dd51df4ef7312a0
bielsilva713 <82852954+bielsilva713@users.noreply.github.com>
2024-06-02 15:19:14 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -50,9 +50,12 @@ definir_mensagens
 echo "$MSG_PASSO_1"
 sudo apt update -y
 
+# Instalar jq para parsear JSON
+sudo apt-get install jq -y
+
 # Passo 2
 echo "$MSG_PASSO_2"
-LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/ant-media/Ant-Media-Server/releases/latest | jq -r '.assets[0].browser_download_url')
+LATEST_RELEASE_URL=$(curl -s https://api.github.com/repos/ant-media/Ant-Media-Server/releases/latest | jq -r '.assets[] | select(.name | test(".*.zip$")) | .browser_download_url')
 wget $LATEST_RELEASE_URL
 
 # Passo 3
